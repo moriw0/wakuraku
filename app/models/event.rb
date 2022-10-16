97ac9d50c70d81453ea51cad5a44d78a1db95822
shota <tontonka2@gmail.com>
2022-10-16 19:18:40 +0900

    add event validates

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :place, length: { maximum: 100 }, presence: true
+  validates :title, length: { maximum: 100 }, presence: true
+  validates :discription, length: { maximum: 2000 }, presence: true
+  validates :price, length: { maximum: 7 }, presence: true
+  validates :required_time, length: { maximum: 3 }, presence: true
+  validates :capacitiy, length: { maximum: 3 }, presence: true
+  validates :is_published, presence: true
 end
