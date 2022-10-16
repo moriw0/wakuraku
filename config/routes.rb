63337c40fe677c86fef5417da6a09e0c4e148c9e
shota <tontonka2@gmail.com>
2022-10-16 16:56:29 +0900

    add device and omniauth for quickstart

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
