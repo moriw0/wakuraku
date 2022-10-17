e3ccc7a45c34d9500d9840c69b9534443ca7404d
shota <tontonka2@gmail.com>
2022-10-17 22:59:14 +0900

    change dobule quotation to single for string

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: 'User'
 
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100 }, presence: true
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Wakuraku
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.time_zone = "Tokyo"
+    config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
