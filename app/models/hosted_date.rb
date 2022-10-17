7c976b5b894cf4498960c21e693e85581471378b
shota <tontonka2@gmail.com>
2022-10-18 08:56:50 +0900

    add hosted_date model

--- app/models/hosted_date.rb
+++ app/models/hosted_date.rb
@@ -1,3 +1,17 @@
 class HostedDate < ApplicationRecord
   belongs_to :event
+
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validate :srart_at_should_be_before_end_at
+
+  private
+
+  def srart_at_should_be_before_end_at
+    return unless start_at && end_at
+
+    if start_at >= end_at
+      errors.add(:start_at, 'は終了時間よりも前に設定してください')
+    end
+  end
 end
