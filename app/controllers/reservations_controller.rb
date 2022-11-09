c58d9f1a30ae5ee139f07e6ddbbf511646d01e2a
shota <tontonka2@gmail.com>
2022-11-09 19:22:54 +0900

    add reservation basic flow

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,16 +1,20 @@
 class ReservationsController < ApplicationController
-  def new
+  def new(event_id:, date_id:)
+    @event = Event.find(event_id)
+    @date = HostedDate.find(date_id)
+    @reservation = current_user.reservations.build
   end
 
-  def create(id:)
-    event = Event.find(id)
+  def create(event_id:, date_id:)
+    event = Event.find(event_id)
+    date = HostedDate.find(date_id)
     @reservation = current_user.reservations.build do |t|
       t.event = event
       t.comment = params[:reservation][:comment]
     end
 
     if @reservation.save
-      redirect_to event, notice: 'イベントに参加しました'
+      redirect_to root_path, notice: 'イベントに参加しました'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :events
+  resources :events do
+    resources :reservations
+  end
 end
