fbd1dafa8550840b54da3f05fb9604833a09a46a
shota <tontonka2@gmail.com>
2022-11-10 21:23:45 +0900

    define validation for reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,15 +8,19 @@ class ReservationsController < ApplicationController
   end
 
   def create(event_id:, date_id:, reservation:)
-    event = Event.find(event_id)
+    @event = Event.find(event_id)
     date = HostedDate.find(date_id)
     @reservation = current_user.reservations.build(reservation) do |t|
-      t.event = event
+      t.event = @event
       t.hosted_date = date
     end
 
     if @reservation.save
       redirect_to root_path, notice: 'イベントに参加しました'
+    else
+      flash.now[:error] = 'イベントに参加できませんでした'
+      render 'events/show'
+      # render 'events/show', notice: "この日時(#{l(date.started_at, format: :long)}-#{l(date.ended_at, format: :short)})には参加できません"
     end
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,9 @@ class Reservation < ApplicationRecord
   belongs_to :hosted_date
 
   validates :comment, length: { maximum: 200 }, allow_blank: true
+  with_options uniqueness: true, presence: true do
+    validates :user_id
+    validates :event_id
+    validates :hosted_date_id
+  end
 end
