ca23332e78793eff565b3b1cd45597d220fd4c42
shota <tontonka2@gmail.com>
2022-11-11 09:00:06 +0900

    refactoring

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,20 +1,18 @@
 class ReservationsController < ApplicationController
+  before_action :set_event_and_date, only: [:new, :create]
   permits :comment
-
-  def new(event_id:, date_id:)
-    @event = Event.find(event_id)
-    @date = HostedDate.find(date_id)
+  
+  def new
     @reservation = current_user.reservations.build
   end
-
-  def create(event_id:, date_id:, reservation:)
-    @event = Event.find(event_id)
-    date = HostedDate.find(date_id)
+  
+  def create(reservation:)
     @reservation = current_user.reservations.build(reservation) do |t|
       t.event = @event
-      t.hosted_date = date
+      t.hosted_date = @date
     end
-
+    
+    debugger
     if @reservation.save
       # TODO: 参加したイベント一覧実装後は遷移先を変更する
       redirect_to root_path, notice: 'イベントに参加しました'
@@ -22,5 +20,12 @@ class ReservationsController < ApplicationController
       flash.now[:error] = 'イベントに参加できませんでした'
       render 'events/show'
     end
+  end
+
+  private 
+  
+  def set_event_and_date(event_id:, date_id:)
+    @event = Event.find(event_id)
+    @date = HostedDate.find(date_id)
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,9 +4,9 @@ class Reservation < ApplicationRecord
   belongs_to :hosted_date
 
   validates :comment, length: { maximum: 200 }, allow_blank: true
-  with_options uniqueness: true, presence: true do
+  with_options presence: true do
     validates :user_id
     validates :event_id
-    validates :hosted_date_id
+    validates :hosted_date_id, uniqueness: true 
   end
 end
