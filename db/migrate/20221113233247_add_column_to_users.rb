12f751050f26e4d9f9577d772dba0353b034245e
moriw0 <87155363+moriw0@users.noreply.github.com>
2022-11-14 19:08:36 +0900

    Revert "ユーザー情報を表示"

--- db/migrate/20221113233247_add_column_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :phone_number, :string
-    add_column :users, :nick_name, :string
-    add_column :users, :profile, :text
-  end
-end
