8d6047bfe1cadf47aaa86afc857604f1642d37e0
shota <tontonka2@gmail.com>
2022-11-14 18:52:37 +0900

    show-user-information
    add user link in header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, only: :show
+
   resources :events do
     resources :reservations
   end
