7d071842f7107dff71606e7de85c2f120c247520
shota <tontonka2@gmail.com>
2022-11-18 08:53:23 +0900

    add edit reservation function

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,7 @@
 class ReservationsController < ApplicationController
-  before_action :set_event_and_date, only: [:new, :create]
+  before_action :set_event, only: [:new, :create]
+  before_action :set_date, only: [:new, :create]
+  before_action :set_reservation, only: [:show, :update, :destroy]
   permits :comment
   
   def index
@@ -17,18 +19,43 @@ class ReservationsController < ApplicationController
     end
     
     if @reservation.save
-      # TODO: 参加したココロミ一覧実装後は遷移先を変更する
-      redirect_to root_path, notice: 'ココロミを予約しました'
+      redirect_to user_reservations_path(current_user), notice: 'ココロミを予約しました'
     else
       flash.now[:error] = 'ココロミを予約できませんでした'
       render 'events/show'
     end
   end
 
+  def show
+    @event = Event.find(@reservation.event_id)
+    @date = HostedDate.find(@reservation.hosted_date_id)
+  end
+
+  def update(event_id:, date_id:)
+    if @reservation.update(event_id: event_id, hosted_date_id: date_id)
+      redirect_to user_reservations_path, notice: '予約を変更しました'
+    else
+      flash.now[:error] = '予約を変更できませんでした'
+      render 'show'
+    end
+  end
+
+  def destroy
+    @reservation.update(is_canceled: true)
+    redirect_to user_reservations_path, notice: '予約をキャンセルしました'
+  end
+
   private 
-  
-  def set_event_and_date(event_id:, date_id:)
+
+  def set_event(event_id:)
     @event = Event.find(event_id)
+  end
+
+  def set_date(date_id:)
     @date = HostedDate.find(date_id)
+  end
+
+  def set_reservation(id:)
+    @reservation = Reservation.find(id)
   end
 end
