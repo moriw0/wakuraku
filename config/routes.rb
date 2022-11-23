8788cf43341d685ff0c00893017f74546f9cffb9
shota <tontonka2@gmail.com>
2022-11-24 00:06:48 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
   }
   resources :users, only: :show do
     get 'reservations/canceled', to: 'reservations#canceled_index', as: 'canceled_reservations'
-    resources :reservations
+    resources :reservations, only: [:index, :show, :update, :destroy]
   end
 
   resources :events do
-    resources :reservations
+    resources :reservations, only: [:new, :create]
+    get 'reservations', to: 'reservations#event_index', as: 'reserved_reservations'
   end
 end
