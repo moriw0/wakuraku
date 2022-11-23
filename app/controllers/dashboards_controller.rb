fd876239f71aa75a0902f2e58e44f3c629c9c959
shota <tontonka2@gmail.com>
2022-11-24 00:06:48 +0900

    add user-reservations

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -11,4 +11,13 @@ class DashboardsController < ApplicationController
   def event_index
     @events = current_user.created_events
   end
+
+  def customer_index
+    @customers = current_user.customers
+  end
+
+  def customer_reservations(id)
+    @customer = User.find(id)
+    @reservations = current_user.reservations_by_customer(id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,26 @@
 Rails.application.routes.draw do
   root 'home#index'
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+
   resources :users, only: :show do
     resources :reservations, only: [:index, :show, :update, :destroy]
     get 'reservations/canceled', to: 'reservations#canceled_index', as: 'canceled_reservations'
-    get 'dashboards/reservations', to: 'dashboards#reservation_index', as: 'dashboard_reservations'
-    get 'dashboards/events', to: 'dashboards#event_index', as: 'dashboard_events'
   end
 
   resources :events do
     resources :reservations, only: [:new, :create]
-    get 'reservations', to: 'dashboards#event_reservations', as: 'dashboard_reservations'
+    get 'reservations', to: 'dashboards#event_reservations'
   end
 
+  scope '/dashboard' do
+    get 'reservations', to: 'dashboards#reservation_index', as: 'dashboard_reservations'
+    get 'events', to: 'dashboards#event_index', as: 'dashboard_events'
+    get 'customers', to: 'dashboards#customer_index', as: 'dashboard_customers'
+    get 'customers/:id', to: 'dashboards#customer_reservations', as: 'customer_reservations'
+  end
 end
