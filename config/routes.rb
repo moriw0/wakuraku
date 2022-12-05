dedf7f52af71af1200bf7db694dd0c0cb084fc96
shota <tontonka2@gmail.com>
2022-12-06 08:29:23 +0900

    modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
   root 'home#index'
+  
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  
   resources :users, only: :show do
     resources :reservations, only: [:index, :show, :update, :destroy]
     get 'reservations/canceled', to: 'reservations#canceled_index', as: 'canceled_reservations'
-    get 'dashboards/reservations', to: 'dashboards#reservation_index', as: 'dashboard_reservations'
-    get 'dashboards/events', to: 'dashboards#event_index', as: 'dashboard_events'
-    get 'dashboards/customers', to: 'dashboards#customer_index', as: 'dashboard_customers'
   end
-
+  
   resources :events do
     resources :reservations, only: [:new, :create]
-    get 'reservations', to: 'dashboards#event_reservations', as: 'dashboard_reservations'
+    get 'reservations', to: 'dashboards#event_reservations'
   end
 
+  scope '/dashboard' do
+    get 'reservations', to: 'dashboards#reservation_index', as: 'dashboard_reservations'
+    get 'events', to: 'dashboards#event_index', as: 'dashboard_events'
+    get 'customers', to: 'dashboards#customer_index', as: 'dashboard_customers'
+  end
 end
