3628f88dda36ad0dd41a1c6e06d3fb8bc167220f
shota <tontonka2@gmail.com>
2023-01-05 19:04:17 +0900

    add pagination for each

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
 
   def show(id:)
     @user = User.find(id)
-    @events = @user.created_events
+    @events = @user.created_events.page params[:page]
   end
 end
