b141fb1fec23582b34f8ec6548b5425097992af5
shota <tontonka2@gmail.com>
2023-01-05 12:36:31 +0900

    introduce kaminari

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @events = Event.with_recent_dates
+    @events = Event.with_recent_dates.page params[:page]
   end
 end
