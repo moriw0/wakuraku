cf26aaecb7e394febe633a5ffc6f18292c7cbd5f
shota <tontonka2@gmail.com>
2023-01-25 20:53:18 +0900

    apply sns image

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,8 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
-      user.image = auth.info.image
+      avatar_url = URI.parse("#{auth.info.image}").open
+      user.avatar.attach(io: avatar_url, filename: "user_avatar.jpg")
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
